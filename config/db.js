12c6dd31d35f0209673889ccd2a79922c66b0647
Tharun <gtharun2511@gmail.com>
2024-02-23 15:30:25 +0530

    initial commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect("mongodb://localhost:27017/realtimechatapp");
+
+        console.log(
+            `MongoDB Connected: ${conn.connection.host}`.cyan.underline
+        );
+    } catch (error) {
+        console.log(`Error: ${error.message}`.red.bold);
+        process.exit();
+    }
+};
+
+module.exports = connectDB;
